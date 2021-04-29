9d7d798632a35a23a4d4ecf8eca2096fba2c1463
Vitor F <vitor.of76@gmail.com>
2021-04-29 22:03:10 +0900

    deploy script

---
+++ linkuru.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo -e "\033[1;35mLink start!\033[0m"
+
+shopt -s extglob
+ignore="@(README*|assets|*arandr)"
+
+for dir in _config/* ; do
+  case "$dir" in
+    $ignore) echo -e "\033[33mWarning:\033[0m Ignoring \033[35m$dir\033[0m" ;;
+    *) [ -d "$HOME/.config" ] && ln -srfv "$dir"  "$HOME/.config" ;;
+  esac
+done
+
+for file in * ; do
+  case "$file" in
+    linkuru.sh) continue ;;
+    $ignore) echo -e "\033[33mWarning:\033[0m Ignoring \033[35m$file\033[0m" ;;
+    *) [ -f "$file" ] && ln -srfv "$file" "$HOME/.${file:1}" ;;
+  esac
+done
